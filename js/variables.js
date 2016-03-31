29925134c7d0252a85641e39b50b6540c59d92d8
Micah Redding <micahtredding@gmail.com>
2016-03-30 23:07:21 -0500

    2016-03-30

---
+++ js/variables.js
@@ -0,0 +1,41 @@
+var current_user = {
+    name: 'John Kelsey',
+    ar:   'AR1212'
+};
+var current_stores = [
+    {
+        id : 1,
+        city : 'Toronto',
+        desc : 'This is the best city in the world!',
+        latitude : 43.7000,
+        longitude : -79.4000
+    },
+    {
+        id : 2,
+        city : 'New York',
+        desc : 'This city is aiiiiite!',
+        latitude : 40.6700,
+        longitude : -73.9400
+    },
+    {
+        id : 3,
+        city : 'Chicago',
+        desc : 'This is the second best city in the world!',
+        latitude : 41.8819,
+        longitude : -87.6278
+    },
+    {
+        id : 4,
+        city : 'Los Angeles',
+        desc : 'This city is live!',
+        latitude : 34.0500,
+        longitude : -118.2500
+    },
+    {
+        id : 5,
+        city : 'Las Vegas',
+        desc : 'Sin City...\'nuff said!',
+        latitude : 36.0800,
+        longitude : -115.1522
+    }
+];
